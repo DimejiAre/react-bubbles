cb817d313dc4b403c5557afc058a3751485c411c
DimejiAre <olardi2@gmail.com>
2019-10-18 10:49:46 +0100

    style login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import * as actionCreators from '../state/actionCreators';
+import './scss/Login.scss';
 
 const Login = (props) => {
   // make a post request to retrieve a token from the api
@@ -12,16 +13,16 @@ const Login = (props) => {
     login(loginForm, history);
   }
   return (
-    <>
+    <div className='login-page'>
       <h1>Welcome to the Bubble App!</h1>
-      <form>
-        <label>username</label>
+      <form className='login-form'>
+        <label>Username</label>
         <input value={loginForm.username} onChange={inputChange} name='username' type='text' placeholder='username'/>
-        <label>username</label>
-        <input value={loginForm.password} onChange={inputChange} name='password' type='password' placeholder='username'/>
+        <label>Password</label>
+        <input value={loginForm.password} onChange={inputChange} name='password' type='password' placeholder='password'/>
         <button type='submit' onClick={submit}>Login</button>
       </form>
-    </>
+    </div>
   );
 };
 
