a1611e87808b252d6d8419dc278843664492a537
DimejiAre <olardi2@gmail.com>
2019-10-18 09:11:54 +0100

    create actionTypes and reducers for loginForm and colors

---
+++ client/src/state/actionTypes.js
@@ -0,0 +1,2 @@
+export const ON_LOGIN_FORM_CHANGE = 'ON_LOGIN_FORM_CHANGE';
+export const ON_SUBMIT = 'ON_SUBMIT';
---
+++ client/src/state/reducers.js
@@ -0,0 +1,28 @@
+import * as types from './actionTypes';
+
+const initialLoginForm = {
+    username: '',
+    password: ''
+}
+
+const initialColors = []
+
+export const colorsReducer = (state=initialColors, action) => {
+    switch(action.type){
+        default:
+        return state;
+    }
+}
+
+export const loginFormReducer = (state = initialLoginForm,action) => {
+    switch(action.type){
+        case types.ON_LOGIN_FORM_CHANGE:
+            return {
+                ...state, [action.payload.key]: action.payload.value
+            }
+        case types.ON_SUBMIT:
+            return initialLoginForm
+        default:
+            return state
+    }
+}
