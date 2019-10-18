d9f5b36486f4831454c5274a69bc20e4c27aa1a9
DimejiAre <olardi2@gmail.com>
2019-10-18 09:12:59 +0100

    create LoginForm component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreators';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const {inputChange, loginForm, login, history} = props;
+
+  const submit = event => {
+    event.preventDefault();
+    login(loginForm, history);
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <label>username</label>
+        <input value={loginForm.username} onChange={inputChange} name='username' type='text' placeholder='username'/>
+        <label>username</label>
+        <input value={loginForm.password} onChange={inputChange} name='password' type='password' placeholder='username'/>
+        <button type='submit' onClick={submit}>Login</button>
+      </form>
     </>
   );
 };
 
-export default Login;
+export default connect(
+  state => state,
+  actionCreators
+)(Login);
